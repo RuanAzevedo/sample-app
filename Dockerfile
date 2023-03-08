fb6c017919450d06b0ac5ec94d475dea074f52f6
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-03-08 10:48:04 -0300

    ♻️ Update code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM node:alpine
 
+EXPOSE 80
+ENV PORT=80
+
+RUN mkdir -p /home/node/app
+WORKDIR /home/node/app
+
 COPY . .
+RUN chown -R node:node .
+
+USER node
 RUN npm install
 ENTRYPOINT [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const app = require("express")();
 
-const PORT = 4567;
-
 app.get("/", (_, res) => {
   return res.send({ version: "v4" });
 });
 
-app.listen(PORT, () => {
-  console.log(`server running at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server running at http://localhost:${process.env.PORT}`);
 });
